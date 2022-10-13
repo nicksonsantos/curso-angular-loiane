e177adb05f271198035f388754faa10dd669fd72
Nickson Santos Endlich <nickson12@gmail.com>
2022-10-13 18:19:44 -0300

    Módulo: Serviços

---
+++ 05_servicos/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
+  ],
+  providers: [
+    CursosService,
+    LogService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 05_servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { LogService } from '../shared/log.service';
+
+@Injectable()
+export class CursosService {
+
+  emitirCursoCriado = new EventEmitter<string>();
+  static criouNovoCurso = new EventEmitter<string>();
+
+  private cursos: string[] = ['Angular', '.NET', 'Python'];
+
+  constructor(private _logService: LogService) {
+    console.log('CursosService');
+  }
+
+  getCursos() {
+    this._logService.consoleLog('Obtendo lista de cursos');
+    return this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this._logService.consoleLog(`Criando um novo curso ${curso}`);
+    this.cursos.push(curso);
+    this.emitirCursoCriado.emit(curso);
+    CursosService.criouNovoCurso.emit(curso);
+  }
+}
