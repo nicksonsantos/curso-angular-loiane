4778fc4ff1174a669e37ebcf772306c2b816eb11
nicksonsantos <nickson12@gmail.com>
2022-10-19 13:21:16 -0300

    Rotas: Tela de Login e como não mostrar a NavBar

--- 07_rotas/src/app/app.module.ts
+++ 07_rotas/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AuthService } from './login/auth.service';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -15,10 +17,11 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
     //CursosModule,
     //AlunosModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 07_rotas/src/app/login/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+
+  nome: string;
+  senha: string;
+
+  constructor() {
+    this.nome = '';
+    this.senha = '';
+  }
+
+}
