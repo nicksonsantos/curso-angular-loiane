18204af3cd832098f4a9bfa76c5b77bd52f37886
nicksonsantos <nickson12@gmail.com>
2022-11-03 13:15:30 -0300

    Verificar validação dos campos com botão submit

--- 08_forms/src/app/data-form/data-form.component.ts
+++ 08_forms/src/app/data-form/data-form.component.ts
@@ -46,14 +46,31 @@ export class DataFormComponent implements OnInit {
 
   onSubmit() {
 
-
-
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+    if (this.formulario.valid) {
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
       .subscribe(dados => {
         console.log(dados);
         // reseta o form
         this.resetar();
       }, (error: any) => alert('erro'));
+    } else {
+      console.log('formulario invalido');
+      this.verificaValidacoesForm(this.formulario);
+    }
+
+
+  }
+
+  verificaValidacoesForm(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((campo) => {
+      const controle = formGroup.get(campo);
+      if (controle instanceof FormGroup) {
+        this.verificaValidacoesForm(controle)
+      }
+      if (controle?.invalid) {
+        controle.markAsDirty();
+      }
+    })
   }
 
   resetar() {
@@ -68,7 +85,7 @@ export class DataFormComponent implements OnInit {
   }
 
   verificaValidTouched(campo: string): boolean {
-    let validacao = this.formulario.get(campo)?.invalid && this.formulario.get(campo)?.touched;
+    let validacao = this.formulario.get(campo)?.invalid && (this.formulario.get(campo)?.touched || this.formulario.get(campo)?.dirty);
 
     if (typeof(validacao) == 'boolean') {
       return validacao;
