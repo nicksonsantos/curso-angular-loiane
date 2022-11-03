9f15d0013ac8471977aae7af159e51479da424c7
nicksonsantos <nickson12@gmail.com>
2022-11-03 14:48:30 -0300

    Commit

---
+++ 08_forms/src/app/shared/services/dropdown.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DropdownService {
+
+  constructor(private http: HttpClient) { }
+
+  getEstadosBr() {
+    this.http.get('assets/dados/estadosbr.json').subscribe()
+
+  }
+}
