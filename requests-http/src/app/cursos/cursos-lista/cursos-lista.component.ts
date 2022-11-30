a28f43ea4f93d22c074a1bb7aab8e1896df6dc7c
nicksonsantos <nickson12@gmail.com>
2022-11-30 14:56:06 -0300

    #132: Http DELETE: Popup de Confirmação p/ remover

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { catchError, EMPTY, Observable, Subject } from 'rxjs';
 import { Curso } from 'src/app/shared/models/curso';
 import { CursosService } from '../cursos.service';
 import { AlertModalService } from '../../shared/alert-modal.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -11,14 +12,22 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./cursos-lista.component.scss']
 })
 export class CursosListaComponent implements OnInit {
-  //bsModalRef?: BsModalRef;
+  deleteModalRef?: BsModalRef;
+  @ViewChild('deleteModal') deleteModal: any;
+  cursoSelecionado: Curso = {id: 0, nome: ''};
 
   // cursos: Curso[] = [];
   error$ = new Subject<boolean>();
   cursos$: Observable<Curso[]> = EMPTY;
 
 
-  constructor(private cursosService: CursosService, private alertModalService: AlertModalService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private cursosService: CursosService,
+    private alertModalService: AlertModalService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit(): void {
     // this.cursosService.getCursos().subscribe((cursos: Curso[]) => this.cursos = cursos);
@@ -47,6 +56,24 @@ export class CursosListaComponent implements OnInit {
 
   onEdit(id: number) {
     this.router.navigate(['editar', id], {relativeTo: this.route});
+  }
+
+  onDelete(curso: Curso) {
+    this.cursoSelecionado = curso;
+    this.deleteModalRef = this.modalService.show(this.deleteModal, {class: 'modal-sm'});
+  }
+
+  onConfirmDelete(): void {
+    this.cursosService.deleteCurso(this.cursoSelecionado.id)
+      .subscribe({
+        next: (v) => this.onRefresh(),
+        error: (e) => this.alertModalService.showAlertDanger('Erro ao remover curso, tente novamente mais tarde.')
+      });
+    this.deleteModalRef?.hide();
+  }
+
+  onDeclineDelete(): void {
+    this.deleteModalRef?.hide();
   }
 
 }
