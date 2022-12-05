ed663ff41a957ea188ef7df414a82a7a9cb1267f
nicksonsantos <nickson12@gmail.com>
2022-12-05 13:46:08 -0300

    #141: Http: Download de Arquivo

--- requests-http/src/app/upload-file/upload-file.service.ts
+++ requests-http/src/app/upload-file/upload-file.service.ts
@@ -26,4 +26,44 @@ export class UploadFileService {
       reportProgress: true
     });
   }
+
+  download(url: string) {
+    return this.http.get(url, {
+      responseType: 'blob' as 'json',
+      // o backend precisa setar o content-lenght para funcionar o reportProgress
+      // reportProgress: true
+    });
+  }
+
+  handleFile(res: any, fileName?: string) {
+    const file = new Blob([res], {
+      type: res.type
+    });
+
+    // IE
+    if (window.navigator && (window.navigator as any).msSaveOrOpenBlob) {
+      (window.navigator as any).msSaveOrOpenBlob(file);
+      return;
+    }
+
+    const blob = window.URL.createObjectURL(file);
+
+    const link = document.createElement('a');
+    link.href = blob;
+    fileName? link.download = fileName : null;
+
+    // funciona no chrome mas nao no firefox
+    // link.click();
+    // para funcionar no firefox fazer:
+    link.dispatchEvent(new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    }));
+
+    setTimeout(() => { // firefox
+      window.URL.revokeObjectURL(blob);
+      link.remove();
+    }, 100);
+  }
 }
